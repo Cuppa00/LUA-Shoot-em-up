0d1297c8766b8d166e1a2bb0ec9dbef624cdfea3
Cuppa00 <callumalanmonk@gmail.com>
2024-02-27 10:51:33 +0000

    New functions

--- main.lua
+++ main.lua
@@ -4,20 +4,24 @@ function _init()	--initialise game (run once)
 	init_player()
 	init_missile()
 	init_enemies()
+	init_starfield()
 end
 
 function _update()	--update game (runs 30 times a second)
-    time += 1
-	move_player()
+    time += 1		--time counter
+	update_player()
 	update_missile()
 	update_enemies()
+	update_starfield()
 end
 
 function _draw()	--called once per visible frame
 	cls(0)
-	draw_player()
+	draw_starfield()
 	draw_missile()
 	draw_enemy()
+	rectfill(0,120,128,128,0)	--fill box behind lives indicator
+	draw_player()
 	draw_lives(lives)
 	if false then
 		debug()
