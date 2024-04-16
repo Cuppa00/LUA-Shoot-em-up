9b3382b15a453a76d2d75fe5758ccdbdd8d37e8a
BuildTools <unconfigured@null.spigotmc.org>
2024-04-16 22:33:36 +0100

    Enemy to player collision added

--- modules/entities/player_mov_col.lua
+++ modules/entities/player_mov_col.lua
@@ -1,11 +1,16 @@
 --player movement, draw & collison
 function init_player() --initial player values
-	pl = {}		--empty player table
-	pl.w = 16	--width
-	pl.x = 56	--start location
-	pl.s = 3	--player speed
-	lives = 3
-
+	pl = {	--player table
+		w = 16,	--width
+		x = 56,	--start location
+		y = 104,
+		s = 3,	--player speed
+		res_t = 0, --respawn timer
+		lives = 3,
+		godmode = false,
+		destroyed = false
+	}
+	
 	flame_time = 0
 end
 
@@ -28,11 +33,15 @@ function update_player() --movement
 	if map_collide(pl.x, pl.w) then	--call collision function
 		pl.x = pl.ox	--if function return true then set players x to old x
 	end
+
+	respawn()
 end
 
 function draw_player()
-	spr(1, pl.x, 104, 2, 2)		--draw 4 sprites for 16x16 sprite
-	spr(flame_spr, pl.x, 120, 2, 1)	--draw engine exhaust flames
+	if pl.destroyed != true then
+		spr(1, pl.x, 104, 2, 2)		--draw 4 sprites for 16x16 sprite
+		spr(flame_spr, pl.x, 120, 2, 1)	--draw engine exhaust flames
+	end
 end
 
 function map_collide(x, w)	--collision
@@ -43,4 +52,59 @@ function map_collide(x, w)	--collision
 	else
 		return false
 	end	  
+end
+
+function ship_en_col(en, pl)
+	if pl.godmode != true then
+		if (en.x<=pl.x+16 and en.x+4>=pl.x) and (en.y+4>104 and en.y<=120) then
+			return true
+		else
+			return false
+		end
+	else
+		return false
+	end
+end
+
+function ship_dead()
+	pl.destroyed = true
+	pl.x = 500
+	pl.y = 500
+	pl.lives -= 1
+end
+
+waiting = false
+enemies_waiting = 0
+function respawn()
+	if pl.destroyed == true then
+		for en in all(enemies) do
+			if en.mission == 'wait' then
+				enemies_waiting += 1
+			end
+			if enemies_waiting == count(enemies) then
+				waiting = true
+			elseif enemies_waiting > count(enemies) then
+				enemies_waiting = 0
+			end
+		end
+
+		if pl.lives != 0 and waiting == true then
+			if pl.res_t == 75 then
+				pl.destroyed = false
+				waiting = false
+				pl.res_t = 0
+				pl.x = 56
+				pl.y = 104
+			else
+				pl.res_t += 1
+			end
+		elseif pl.lives == 0 then
+			if pl.res_t == 45 then
+				pl.res_t = 0
+				gameend = true
+			else
+				pl.res_t += 1
+			end
+		end
+	end
 end
