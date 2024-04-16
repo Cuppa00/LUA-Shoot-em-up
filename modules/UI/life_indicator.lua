693e334fade8231793943de6e771ace1acbc8af5
BuildTools <unconfigured@null.spigotmc.org>
2024-04-16 22:34:38 +0100

    Lives now decrease if player hit

--- modules/UI/life_indicator.lua
+++ modules/UI/life_indicator.lua
@@ -1,9 +1,11 @@
 --lives indicator
 function draw_lives(l)	--pass in amount of lives
-	livesx = 0	--lives sprite indicator x starts at 0
-	repeat
-		spr(6, livesx, 120)	--draw sprite at 120 (bottom of screen)
-		livesx+=8	--add 8 (width of sprite) so next sprite will be drawn to its side
-		l -= 1	--take away from loop
-	until l == 0	--end loop when l=0
+	if l != 0 then
+		livesx = 0	--lives sprite indicator x starts at 0
+		repeat
+			spr(6, livesx, 120)	--draw sprite at 120 (bottom of screen)
+			livesx+=8	--add 8 (width of sprite) so next sprite will be drawn to its side
+			l -= 1	--take away from loop
+		until l == 0	--end loop when l=0
+	end
 end
