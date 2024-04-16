d6ac0bc83127d0a4859f788c97fd7b445d0d4931
BuildTools <unconfigured@null.spigotmc.org>
2024-04-16 22:31:52 +0100

    Added end game functionality

--- main.lua
+++ main.lua
@@ -6,15 +6,20 @@ function _init()	--initialise game (run once)
 	init_enemies()
 	init_starfield()
 	init_explosion()
+	gameend = false
 end
 
 function _update()	--update game (runs 30 times a second)
     time += 1		--time counter
-	update_player()
-	update_missile()
-	update_enemies()
-	update_starfield()
+	if gameend != true then
+		update_player()
+		update_missile()
+		update_enemies()
+	elseif gameend == true then
+		update_fade()
+	end
 	update_explosion()
+	update_starfield()
 	if true then
 		update_debug()
 	end
@@ -22,14 +27,19 @@ end
 
 function _draw()	--called once per visible frame
 	cls(0)
-	draw_starfield()
-	draw_missile()
-	draw_enemy()
-	rectfill(0,120,128,128,0)	--fill box behind lives indicator
-	draw_player()
-	draw_lives(lives)
-	draw_explosion()
 	if false then
 		draw_debug()
 	end
+	draw_starfield()
+	if gameend != true then
+		draw_missile()
+		draw_enemy()
+		rectfill(0,120,128,128,0)	--fill box behind lives indicator
+		draw_player()
+		draw_lives(pl.lives)
+	else
+		draw_end()
+		draw_fade()
+	end
+	draw_explosion()
 end
